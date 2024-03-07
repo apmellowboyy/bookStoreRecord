24b2408610b89828a09f235fd1a51fd248cfd4a2
eaten <eaten@LAPTOP-5H3QD1H9>
2024-03-07 09:17:16 -0500

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace bruh
+{
+    class Program
+    {
+        public record Bookstore(string Title, string Author, double Price);
+        public static void Main()
+        {
+            Bookstore Bookstore = new("Holes", "Louis Sachar", 4.99);
+            Console.WriteLine(Bookstore);
+
+            Bookstore yerddd = new("Attack on Titan", "Hajime Isayama", 5.99);
+            Console.WriteLine(yerddd);
+
+            Bookstore cmonDawg = new("The Hunger Games", "Suzanne Collins", 7.99);
+        }
+    }
+}
